a71f2911e0a42d9ba410a79b3e673f04737432a1
Clayton Knapp <csjknapp@gmail.com>
2022-04-12 18:01:16 -0700

    fixed some eslint errors

--- scrape2.js
+++ scrape2.js
@@ -1,23 +1,23 @@
 const cron = require('node-cron');
 
 //const puppeteer = require('puppeteer')
-const puppeteer = require('puppeteer-extra')
+const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-var fs = require('fs');
+const fs = require('fs');
 
-var arrayOfItems;
+let arrayOfItems;
 
 // UPDATE WITH YOUR LOCATION REFERENCE FROM STEP 4
-let locationRef = 'portland';
+const locationRef = 'portland';
 
 // UPDATE WITH ITEMS YOU WANT TO SEARCH FOR
-let searchTerms = ['Mid-Century', 'Mid Century'];
+const searchTerms = ['Mid-Century', 'Mid Century'];
 
 const nodemailer = require('nodemailer');
 
 // UPDATE WITH EMAIL YOU WANT TO RECEIVE AT
-let emailRecipient = "ClaytonSKnapp@gmail.com";
+const emailRecipient = 'ClaytonSKnapp@gmail.com';
 
 // UPDATE WITH YOUR SENDING EMAIL ACCOUNT
 const transporter = nodemailer.createTransport({
@@ -30,13 +30,13 @@ const transporter = nodemailer.createTransport({
 
 // UPDATE WITH YOUR SENDING EMAIL ACCOUNT
 function sendEmail(emailRecipient, searchTerm, items){
-  var message = "See new items below: \n\n"
-  for (var a=0;a<items.length;a++){
-    var item_string = `${items[a].title} - ${items[a].price}\n${items[a].link}\n\n`;
-    message = message + item_string
+  let message = ';See new items below: \n\n';
+  for (let a = 0; a < items.length; a++){
+    const item_string = `${items[a].title} - ${items[a].price}\n${items[a].link}\n\n`;
+    message = message + item_string;
   }
   const mailOptions = {
-	  // UPDATE WITH YOUR SENDING EMAIL ACCOUNT
+    // UPDATE WITH YOUR SENDING EMAIL ACCOUNT
     from: '"Marketplace Alert" change@gmail.com',
     to: emailRecipient,
     subject: `${items.length} new items listed under ${searchTerm}`,
@@ -54,34 +54,34 @@ function sendEmail(emailRecipient, searchTerm, items){
 async function getItems(){
   fs.readFile('./pastItems.json', 'utf-8', function(err, data) {
     arrayOfItems = JSON.parse(data);
-  })
-  const browser = await puppeteer.launch({headless: true})
-  const page = await browser.newPage()
-  for (var i=0;i<searchTerms.length;i++){
-    var newItems = [];
-    var searchTerm = searchTerms[i].replace(/ /g,'%20');    
-    console.log(`\nResults for ${searchTerms[i]}:\n`)
+  });
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  for (let i = 0; i < searchTerms.length; i++){
+    const newItems = [];
+    const searchTerm = searchTerms[i].replace(/ /g, '%20');    
+    console.log(`\nResults for ${searchTerms[i]}:\n`);
     await page.waitForTimeout(5000);
-    await page.goto(`https://www.facebook.com/marketplace/${locationRef}/search/?daysSinceListed=1&sortBy=best_match&query=${searchTerm}&exact=false`)
+    await page.goto(`https://www.facebook.com/marketplace/${locationRef}/search/?daysSinceListed=1&sortBy=best_match&query=${searchTerm}&exact=false`);
     //console.log(`\n1\n`)
     await page.waitForTimeout(5000);
-    let bodyHTML = await page.evaluate(() => document.body.outerHTML);
+    const bodyHTML = await page.evaluate(() => document.body.outerHTML);
     //console.log(`\n2\n`)
     await page.waitForTimeout(5000);
     try {
-      let searchResult = JSON.parse(bodyHTML.split(/(?:"marketplace_search":|,"marketplace_seo_page")+/)[2]);
+      const searchResult = JSON.parse(bodyHTML.split(/(?:"marketplace_search":|,"marketplace_seo_page")+/)[2]);
       //console.log(`\n3\n`)
-      await page.waitForTimeout(5000)
-      let items = searchResult["feed_units"]["edges"];
+      await page.waitForTimeout(5000);
+      const items = searchResult['feed_units']['edges'];
       //console.log(`\n4\n`)
       await page.waitForTimeout(5000);
       if (items.length > 1){
-        items.forEach((val, index)=>{
-          var ID1 = val['node']['listing']['id'];
-          var link = `https://www.facebook.com/marketplace/item/${val['node']['listing']['id']}`;
-          var title = val['node']['listing']['marketplace_listing_title'];
-          var price = val['node']['listing']['listing_price']['formatted_amount'];
-          var item = {title: title, price: price, link: link}
+        items.forEach((val, index) => {
+          const ID1 = val['node']['listing']['id'];
+          const link = `https://www.facebook.com/marketplace/item/${val['node']['listing']['id']}`;
+          const title = val['node']['listing']['marketplace_listing_title'];
+          const price = val['node']['listing']['listing_price']['formatted_amount'];
+          const item = { title, price, link };
           if (arrayOfItems.pastItems.includes(ID1)){
           } else {
             arrayOfItems.pastItems.push(ID1);
@@ -104,7 +104,7 @@ async function getItems(){
   fs.writeFile('./pastItems.json', JSON.stringify(arrayOfItems), 'utf-8', function(err) {
     if (err) throw err;
     console.log('Updated past items');
-  })
+  });
 }
 
 // TO CHANGE CRON TIME SCHEDULE
